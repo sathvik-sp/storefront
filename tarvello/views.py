776b8ffe8d506a55c10de187e7364e0d23fe1dca
sathvik-sp <sathviksp123@gmail.com>
2024-07-09 00:07:00 +0530

    created a simple dynamic page with 2 dest objects

--- tarvello/views.py
+++ tarvello/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 
 
 def index(request):
     #return HttpResponse("hello world")
-    return render(request,'index.html')
+    dest1=Destination()
+    dest1.name="Mumbai"
+    dest1.desc="The city that never sleeps"
+    dest1.price=800
+
+    dest2=Destination()
+    dest2.name="Delhi"
+    dest2.desc="The capital city of India"
+    dest2.price=1800
+    
+    return render(request,'index.html',{'dest1':dest1,'dest2':dest2})
