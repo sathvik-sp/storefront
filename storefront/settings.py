5bfde9226f3cd1b45e12d5effcbf4daa194cf416
sathvik-sp <sathviksp123@gmail.com>
2024-07-14 23:08:53 +0530

    ORM, creating a model, migration, remigration, registerning admin model to add data

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sessions',
     'playground',
-    'tarvello'
+    'tarvello.apps.TarvelloConfig'
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storefront',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
     }
 }
 
