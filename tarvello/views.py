a6aebe2cc12b9ca1744cc1210828bdf7b65efd1e
sathvik-sp <sathviksp123@gmail.com>
2024-08-04 20:49:28 +0530

    adding and fetching data from db

--- tarvello/views.py
+++ tarvello/views.py
@@ -5,28 +5,28 @@ from .models import Destination
 
 def index(request):
     #return HttpResponse("hello world")
-    dest1=Destination()
-    dest1.name="Mumbai"
-    dest1.desc="The city that never sleeps"
-    dest1.img = 'destination_1.jpg'
-    dest1.price=650
-    dest1.offer=False
+    # dest1=Destination()
+    # dest1.name="Mumbai"
+    # dest1.desc="The city that never sleeps"
+    # dest1.img = 'destination_1.jpg'
+    # dest1.price=650
+    # dest1.offer=False
 
-    dest2=Destination()
-    dest2.name="Bangaluru"
-    dest2.desc="Silicon valley of india"
-    dest2.img = 'destination_2.jpg'
-    dest2.price=750
-    dest2.offer=True
+    # dest2=Destination()
+    # dest2.name="Bangaluru"
+    # dest2.desc="Silicon valley of india"
+    # dest2.img = 'destination_2.jpg'
+    # dest2.price=750
+    # dest2.offer=True
 
-    dest3=Destination()
-    dest3.name="Hyderabad"
-    dest3.desc="Land of Biryani"
-    dest3.img = 'destination_3.jpg'
-    dest3.price=700
-    dest3.offer=True
+    # dest3=Destination()
+    # dest3.name="Hyderabad"
+    # dest3.desc="Land of Biryani"
+    # dest3.img = 'destination_3.jpg'
+    # dest3.price=700
+    # dest3.offer=True
 
-    dests=[dest1, dest2, dest3]
+    # dests=[dest1, dest2, dest3]
 
-    
+    dests = Destination.objects.all()
     return render(request,'index.html',{'dests':dests})
